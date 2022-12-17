dd7a671167479722af07a07db9bae1de476a88fb
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2022-12-18 01:09:34 +0300

    Начальный коммит

---
+++ src/courseWork1/Employee.java
@@ -0,0 +1,10 @@
+package courseWork1;
+
+public class Employee {
+    private String fullname;
+    private int department;
+    private int salary;
+    private int id;
+    private static int counterId;
+
+}
