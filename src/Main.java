1660f34cf591bfd5e7be5cd054611c944d267f49
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2022-12-18 02:18:28 +0300

    На стадии создания статических методов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import courseWork1.Employee;
+
 public class Main {
+    private static Employee[] employees = new Employee[10];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        employees[0] = new Employee("ТЕСТ1", 1, 45000);
+        employees[1] = new Employee("ТЕСТ2", 2, 45000);
+        employees[2] = new Employee("ТЕСТ3", 2, 75000);
+        employees[3] = new Employee("ТЕСТ4", 2, 45600);
+        printEmployeesInfo();
     }
+    private static void printEmployeesInfo() {
+        for (Employee employee : employees) {
+            if (employee == null) {
+                break;
+            }
+            System.out.println(employee.toString());
+            System.out.println();
+        }
+    }
+
+
+
 }
--- src/courseWork1/Employee.java
+++ src/courseWork1/Employee.java
@@ -7,4 +7,40 @@ public class Employee {
     private int id;
     private static int counterId;
 
+    public Employee(String fullname, int department, int salary) {
+        if (department < 0 || department > 5) {
+            throw new IllegalArgumentException("Такого отдела не существует");
+        }
+        this.fullname = fullname;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++counterId;
+    }
+    public String getFullname() {
+        return fullname;
+    }
+    public int getDepartment(){
+        return department;
+    }
+    public int getSalary(){
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartament(int department) {
+        this.department = department;
+    }
+    @Override
+    public String toString () {
+        String infoEmployees  = String.format("id Сотрудника :%d Сотрудник : %s, Отдел №%d, Зароботная плата: %d",id, fullname, department, salary);
+        return infoEmployees;
+    }
+
 }
